3b50a0791df2576b610fa6c7e4cbb932c08ddada
tanyakamboj123 <21f3000745@ds.study.iitm.ac.in>
2025-01-31 21:13:07 +0530

    Update index.py

--- index.py
+++ index.py
@@ -4,7 +4,19 @@ from http.server import BaseHTTPRequestHandler
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','application/json')
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')  # Enable CORS
         self.end_headers()
-        self.wfile.write(json.dumps({"message": "Hello!"}).encode('utf-8'))
-        return
+
+        names = self.parse_query_parameters()
+        marks = {"marks": [10, 20]}  # Example marks
+        self.wfile.write(json.dumps(marks).encode('utf-8'))
+
+    def parse_query_parameters(self):
+        query = self.path.split('?')[-1]
+        query_dict = {}
+        if query:
+            for param in query.split('&'):
+                key, value = param.split('=')
+                query_dict[key] = value
+        return query_dict.get('name', [])
